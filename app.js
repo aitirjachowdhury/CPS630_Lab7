184771808ecc07b82922346ea09ab5f553d7e8d3
Felicia Levina <felicialevina10@gmail.com>
2021-03-24 19:28:09 +0800

    4, 5, 6, 7, 8, 9

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var app = angular.module('myApp', []);
-app.controller('HomeController', function($scope)
-{
-  $scope.message = 'Hello from HomeController';
+var app = angular.module('myApp', ['ngRoute']);
+app.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {templateUrl : 'home.html',controller : 'HomeController'})
+  .when('/aboutus', {templateUrl : 'aboutus.html',controller : 'AboutusController'})
+  .when('/reviews', {templateUrl : 'review.html',controller : 'ReviewsController'})
+  .otherwise({redirectTo: '/'});
 });
+
+app.controller('HomeController', function($scope) {
+  $scope.message = 'Hello from HomeController';});
+app.controller('AboutusController', function($scope) {
+  $scope.message = 'Hello from AboutusController';});
+app.controller('ReviewsController', function($scope) {
+  $scope.message = 'Hello from ReviewsController';});
